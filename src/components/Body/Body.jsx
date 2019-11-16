e70b1bf974c7af2dd3f78ae42c3d3cd3e4434765
Glyn Wei <glynk.wei@gmail.com>
2019-11-15 19:57:57 -0500

    Whitespace changes

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import './Body.css'
 
 export default function Body() {
-    return(
-        <div className="big-bodied-body">
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-			<div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-			<div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-            <div>I have a big body.</div>
-        </div>
-    )
+  return (
+    <div className="big-bodied-body">
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+      <div>I have a big body.</div>
+    </div>
+  )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,55 +1,55 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { HamburgerVortex } from 'react-animated-burgers'
 
 import './Header.css'
 
 export default function Header() {
-	const MINIMUM_WINDOW_WIDTH = 700;
+  const MINIMUM_WINDOW_WIDTH = 700;
 
-	const [buttonState, setButtonState] = useState(false);
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [buttonState, setButtonState] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
-	const updateWindowWidth = () => {
-		if(window.innerWidth < MINIMUM_WINDOW_WIDTH) {
-			setButtonState(false);
-		}
-		setWindowWidth(window.innerWidth);
-	};
-
-	window.addEventListener("resize", updateWindowWidth);
-
-    const toggleButton = () => {
-        setButtonState(!buttonState);
+  const updateWindowWidth = () => {
+    if (window.innerWidth < MINIMUM_WINDOW_WIDTH) {
+      setButtonState(false);
     }
+    setWindowWidth(window.innerWidth);
+  };
 
-    const listItems = ['Link 1', 'Link 2', 'Link 3', 'Link 4', 'Link 5'];
+  window.addEventListener("resize", updateWindowWidth);
 
-	const displayNavigationBar = () => {
-		return (
-			<ul className='header-list-items-horizontal'>
-				{listItems.map(item => <li key={item} className='header-item-horizontal'>{item}</li>)}
-			</ul>
-		)
-	};
+  const toggleButton = () => {
+    setButtonState(!buttonState);
+  }
 
-	const displayHamburger = () => {
-		return (
-			<HamburgerVortex isActive={buttonState} toggleButton={toggleButton}/>
-		)
-	}
+  const listItems = ['Link 1', 'Link 2', 'Link 3', 'Link 4', 'Link 5'];
 
-	const displayHamburgerMenu = () => {
-		return (
-			<ul className='header-list-items-vertical'>
-				{listItems.map(item => <li key={item} className='header-item-vertical'>{item}</li>)}
-			</ul>
-		)
-	};
-
-    return(
-        <div>
-			{windowWidth < MINIMUM_WINDOW_WIDTH ? displayHamburger() : displayNavigationBar()}
-			{buttonState ? displayHamburgerMenu() : null}
-        </div>
+  const displayNavigationBar = () => {
+    return (
+      <ul className='header-list-items-horizontal'>
+        {listItems.map(item => <li key={item} className='header-item-horizontal'>{item}</li>)}
+      </ul>
     )
+  };
+
+  const displayHamburger = () => {
+    return (
+      <HamburgerVortex isActive={buttonState} toggleButton={toggleButton} />
+    )
+  }
+
+  const displayHamburgerMenu = () => {
+    return (
+      <ul className='header-list-items-vertical'>
+        {listItems.map(item => <li key={item} className='header-item-vertical'>{item}</li>)}
+      </ul>
+    )
+  };
+
+  return (
+    <div>
+      {windowWidth < MINIMUM_WINDOW_WIDTH ? displayHamburger() : displayNavigationBar()}
+      {buttonState ? displayHamburgerMenu() : null}
+    </div>
+  )
 }
